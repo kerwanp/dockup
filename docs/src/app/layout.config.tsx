413d74769bd7a83ad8719e98363842436c92339e
Martin Paucot <contact@martin-paucot.fr>
2025-08-23 02:43:58 +0200

    first commit

--- docs/src/app/layout.config.tsx
+++ docs/src/app/layout.config.tsx
@@ -1,3 +1,4 @@
+import { LogoSvg } from "@/components/svgs/logo";
 import { GithubInfo } from "fumadocs-ui/components/github-info";
 import type { BaseLayoutProps, LinkItemType } from "fumadocs-ui/layouts/shared";
 import { AlbumIcon, Package } from "lucide-react";
@@ -23,7 +24,11 @@ export const linkItems: LinkItemType[] = [
 
 export const baseOptions: BaseLayoutProps = {
   nav: {
-    title: <>Dockup</>,
+    title: (
+      <>
+        <div className="font-bold font-mono text-2xl">DOCKUP</div>
+      </>
+    ),
     url: "/",
   },
   // see https://fumadocs.dev/docs/ui/navigation/links

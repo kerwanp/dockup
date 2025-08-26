202627b190ade00da1da02be5058e799a03c748d
Martin Paucot <contact@martin-paucot.fr>
2025-08-26 09:07:17 +0200

    setup ci

--- packages/dockup/src/cli/commands/connect.ts
+++ packages/dockup/src/cli/commands/connect.ts
@@ -1,7 +1,5 @@
-import { defineCommand } from "citty";
 import { loadConfig } from "../../config/load_config.js";
 import { loadDockup } from "../../load_dockup.js";
-import { log } from "@clack/prompts";
 import { prompts } from "../utils.js";
 import { createDockupCommand } from "../create_command.js";
 import { Exception } from "../../exceptions/exception.js";
@@ -28,32 +26,3 @@ export const ConnectCommand = createDockupCommand("connect")
 
     await service.connect();
   });
-
-export default defineCommand({
-  meta: {
-    name: "connect",
-    description: "Connect to a running service",
-  },
-  args: {
-    service: {
-      type: "positional",
-      description: "The service name",
-      required: true,
-    },
-  },
-  async run({ args }) {
-    prompts.intro("dockup connect");
-
-    const config = await loadConfig();
-    const dockup = await loadDockup(config);
-
-    const service = dockup.service(args.service);
-
-    if (!service.connect) {
-      log.error("This service does not support connection");
-      process.exit(1);
-    }
-
-    await service.connect();
-  },
-});
